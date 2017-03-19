287bca9607a00bbaadf3d3305e667b26074e0490
Mike Dalessio <mike.dalessio@gmail.com>
2017-03-19 16:35:52 -0400

    improve linewrap in prelude.ps1

--- src/prelude.ps1
+++ src/prelude.ps1
@@ -7,6 +7,15 @@ Set-PSDebug -trace 1            # "set -x"
 Set-PSDebug -strict             # "set -u"
 $ErrorActionPreference = "Stop" # "set -e"
 
+# avoid overly narrow default linewrap
+$term = (get-host).ui.rawui
+$size = $term.buffersize
+$size.width = 128
+$term.buffersize = $size
+$size = $term.windowsize
+$size.width = 128
+$term.windowsize = $size
+
 function conditional-package-prepend-path {
     param ($pkg)
     $dir = "c:\var\vcap\packages\$($pkg)\bin"
